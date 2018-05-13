5208387571fc6248d32338c521dedf118c7e5f09
75557032 <2524153@163.com>
2018-05-13 16:10:29 +0800

    模板中sizeof...运算符

---
+++ DEMO/sizeoftemplate_test/main.cpp
@@ -0,0 +1,23 @@
+﻿#include <QCoreApplication>
+#include <QDebug>
+#include <array>
+#include <initializer_list>
+
+template<typename ...T>
+static void test(T &&...a)
+{
+    qDebug() << sizeof...(a);
+    auto b = { std::forward<T>(a)... };
+    for (auto it = b.begin(); it != b.end(); ++it) {
+        qDebug() << *it;
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    test<int>(1, 2, 3);
+
+    return a.exec();
+}
