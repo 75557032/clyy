94ce82d18e16cd38d1d556396871128ebf68e1d0
75557032 <2524153@163.com>
2018-05-13 17:47:04 +0800

    位域和大小端，MSB,LSB

---
+++ DEMO/bit_test/main.cpp
@@ -0,0 +1,24 @@
+﻿#include <QCoreApplication>
+#include <QDebug>
+
+struct CTest
+{
+    unsigned short a1 : 2;
+    unsigned short a2 : 7;
+    unsigned short a3 : 7;
+};
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    unsigned short x = 0x0102;
+    CTest *p = (CTest *)&x;
+    qDebug("%x %x %x", p->a1, p->a2, p->a3);
+
+    x = 0x0902;
+    p = (CTest *)&x;
+    qDebug("%x %x %x", p->a1, p->a2, p->a3);
+
+    return a.exec();
+}
