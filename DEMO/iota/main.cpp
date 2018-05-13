c899650e0efc464df64a0dfa916e596af4706ee1
75557032 <2524153@163.com>
2018-05-13 16:22:45 +0800

    iota

---
+++ DEMO/iota/main.cpp
@@ -0,0 +1,19 @@
+﻿#include <QCoreApplication>
+#include <QDebug>
+
+#include <numeric>
+#include <vector>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    std::vector<int> b(5);
+    iota(b.begin(), b.end(), 1);
+
+    for (auto it = b.begin(); it != b.end(); ++it) {
+        qDebug() << *it;
+    }
+
+    return a.exec();
+}
