26eee5bd5f0035536e8afd67287fcc206953251a
75557032 <2524153@163.com>
2018-05-13 15:48:03 +0800

    数组限制元素大小传递

---
+++ DEMO/array_test/main.cpp
@@ -0,0 +1,21 @@
+﻿#include <QCoreApplication>
+
+static void test(char (&xx)[3])
+{
+    //
+}
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    char xx[3];
+    test(xx);
+    char *pxx = new char[3];
+    test(*((char (*)[3])pxx));
+
+    char bxx[5];
+    test(*((char (*)[3])&bxx[1]));
+
+    return a.exec();
+}
